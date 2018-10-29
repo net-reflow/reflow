3121b9a74f6d5931b63cd5ea470f19b5878e3979
lvar0 <43428661+lvar0@users.noreply.github.com>
2018-10-29 22:43:30 +0000

    ipv6 zone

--- src/conf/prefix_match/ip_addr.rs
+++ src/conf/prefix_match/ip_addr.rs
@@ -1,10 +1,8 @@
 extern crate treebitmap;
 
 use std::net::Ipv4Addr;
-use std::collections::HashMap;
 
 use std::fs;
-use std::fs::DirEntry;
 use std::str::FromStr;
 use std::str::from_utf8;
 
@@ -16,16 +14,37 @@ use std::path;
 use std::net::IpAddr;
 use super::util::lines_without_comments;
 use util::BsDisp;
+use std::net::Ipv6Addr;
 
 pub struct IpMatcher {
-    ip4_table: IpLookupTable<Ipv4Addr, Bytes>
+    ip4_table: IpLookupTable<Ipv4Addr, Bytes>,
+    ip6_table: IpLookupTable<Ipv6Addr, Bytes>,
 }
 
 impl IpMatcher {
     pub fn new(confpath: &path::Path) -> Result<IpMatcher, Error> {
         let regions = find_addr_map_files(confpath)?;
+
+        let mut i4table= IpLookupTable::new();
+        let mut i6table= IpLookupTable::new();
+        for (region, conf) in regions {
+            for entry in conf.iter() {
+                let  contents = fs::read(entry.path())?;
+                let ls = lines_without_comments(&contents);
+                for line in ls {
+                    let (a,m) = try_parse_ip_network(line)
+                        .map_err(|e| format_err!(
+                    "Can't parse {} as IP network: {:?}", BsDisp::new(line), e))?;
+                    match a {
+                        IpAddr::V6(a) => i6table.insert(a, m, region.clone()),
+                        IpAddr::V4(a) => i4table.insert(a, m, region.clone()),
+                    };
+                }
+            }
+        }
         Ok(IpMatcher{
-            ip4_table: build_ip4_table(&regions)?,
+            ip4_table: i4table,
+            ip6_table: i6table,
         })
     }
 
@@ -41,35 +60,22 @@ impl IpMatcher {
         match ip {
             IpAddr::V4(i) => self.ip4_table.longest_match(i)
                 .map(|(_i, _m, v)| v.clone()),
-            IpAddr::V6(_i) => None,
+            IpAddr::V6(i) => self.ip6_table.longest_match(i)
+                .map(|(_i, _m, v)| v.clone()),
         }
     }
 }
 
-fn build_ip4_table(regions: &HashMap<Bytes, Vec<DirEntry>>)
-    -> Result<IpLookupTable<Ipv4Addr, Bytes>, Error> {
-    let mut i4table= IpLookupTable::new();
-    for (region, conf) in regions {
-        for entry in conf.iter() {
-            let  contents = fs::read(entry.path())?;
-            let ls = lines_without_comments(&contents);
-            for line in ls {
-                let (a,m) = try_parse_ip_network(line)
-                    .map_err(|e| format_err!(
-                    "Can't parse {} as IP network: {:?}", BsDisp::new(line), e))?;
-                i4table.insert(a, m, region.clone());
-            }
-        }
-    }
-    Ok(i4table)
-}
-
-fn try_parse_ip_network(line: &[u8])-> Result<(Ipv4Addr, u32), Error> {
+fn try_parse_ip_network(line: &[u8])-> Result<(IpAddr, u32), Error> {
     let mut p = line.splitn(2, |&x| x == b'/');
     let a = p.next().ok_or_else(|| format_err!("Not address"))?;
     let m = p.next().ok_or_else(|| format_err!("Not masklen"))?;
     let a = from_utf8(a)?;
-    let a = Ipv4Addr::from_str(a)?;
+    let a = Ipv4Addr::from_str(a)
+        .map(|a| IpAddr::V4(a))
+        .or_else(|_e| {
+            Ipv6Addr::from_str(a).map(|a| IpAddr::V6(a))
+        })?;
     let m = from_utf8(m)?;
     let m = u32::from_str(m)?;
     Ok((a, m))
