2cef567ab01e5be780391bfe4ab56bc390efa9a5
lvar0 <43428661+lvar0@users.noreply.github.com>
2018-10-30 00:40:05 +0000

    namezone test

--- src/conf/prefix_match/domain_name.rs
+++ src/conf/prefix_match/domain_name.rs
@@ -91,20 +91,19 @@ mod tests {
 
     #[test]
     fn test_some_domains() {
-        let p = path::PathBuf::from("config");
-        let f = fs::read_to_string(p.join("domain-region-test")).unwrap();
+        let p = path::PathBuf::from("test/conf.d");
+        let f = fs::read_to_string(p.join("namezone-expectation")).unwrap();
         let r =
-            f.lines().map(|l|-> (String, Option<Bytes>) {
-                let v: Vec<&str> = l.split_whitespace().collect();
-                let d: Vec<&str> = v[0].split('.').rev().collect();
-                let d = d.join(".");
+            f.lines().map(|l|-> (&str, Option<Bytes>) {
+                let v: Vec<&str> = l.trim().split_whitespace().collect();
+                let d = v[0];
                 let r = v.get(1).map(|&x| x.into());
                 (d, r)
             });
         let d = DomainMatcher::new(&p).unwrap();
         for (h, v) in r {
             let j = d.rule_domain(h.as_bytes());
-            assert_eq!(j, v);
+            assert_eq!(j, v, "testing {}", h);
         }
     }
 }
