fec9e48bd360dbfdc011cbb4f289be30ffaa99f3
paracat <44520333+paracat@users.noreply.github.com>
2018-11-16 19:55:31 +0000

    async read socks protocol command

--- src/proto/socks/heads/command.rs
+++ src/proto/socks/heads/command.rs
@@ -64,6 +64,29 @@ pub fn read_command(r: TcpStream, peer: SocketAddr)
         })
 }
 
+pub async fn read_command_async(mut s: TcpStream, peer: SocketAddr)
+    -> Result<(TcpStream, TcpRequestHeader), Error> {
+    let (_s, buf) = await!(read_exact(&mut s, [0u8; 3]))?;
+    let ver = buf[0];
+    if ver != consts::SOCKS5_VERSION {
+        await!(write_command_response_async(&mut s, Reply::ConnectionRefused, peer))?;
+        s.shutdown(Shutdown::Both)?;
+        return Err(SocksError::SocksVersionNoSupport {ver}.into());
+    }
+    let cmd = buf[1].try_into();
+    if let Err(e) = cmd {
+        let e: SocksError = e;
+        await!(write_command_response_async(&mut s, Reply::CommandNotSupported, peer))?;
+        s.shutdown(Shutdown::Both)?;
+        return Err(e.into());
+    }
+    let cmd = cmd.unwrap();
+    await!(write_command_response_async(&mut s, Reply::SUCCEEDED, peer))?;
+    let (s, address) = await!(Address::read_from(s))?;
+    let header = TcpRequestHeader { command: cmd, address };
+    Ok((s, header))
+}
+
 fn write_command_response(s: TcpStream, rep: Reply, addr: SocketAddr) -> impl Future<Item=TcpStream, Error=Error> {
     let addr = Address::SocketAddress(addr);
     let resp = TcpResponseHeader::new(rep, addr);
@@ -73,4 +96,14 @@ fn write_command_response(s: TcpStream, rep: Reply, addr: SocketAddr) -> impl Fu
     write_all(s, buf)
         .map(|(s, _b)| s)
         .map_err(|e| e.into())
+}
+
+async fn write_command_response_async(s: &mut TcpStream, rep: Reply, addr: SocketAddr)
+    -> Result<(), Error> {
+    let addr = Address::SocketAddress(addr);
+    let resp = TcpResponseHeader::new(rep, addr);
+    let mut buf = BytesMut::with_capacity(resp.len());
+    buf.put_slice(&[consts::SOCKS5_VERSION, resp.reply as u8, 0x00]);
+    write_address(&resp.address, &mut buf);
+    await!(write_all(s, buf)).map(|_s| ()).map_err(|e| e.into())
 }
